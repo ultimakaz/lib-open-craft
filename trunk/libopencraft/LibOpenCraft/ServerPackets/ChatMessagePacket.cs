4f68167e8e25771bb541a0dc8ce9d04ce9083efd
admin@glscripting.org <admin@glscripting.org@5e01b60c-9de4-9055-072c-856d3e03f645>
2011-09-20 21:30:26 +0000

    added chat message packet

---
+++ trunk/libopencraft/LibOpenCraft/ServerPackets/ChatMessagePacket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibOpenCraft.ServerPackets
+{
+    public class ChatMessagePacket : PacketHandler
+    {
+        public string MessageRecieved
+        {
+            get;
+            set;
+        }
+        public string MessageSent
+        {
+            get;
+            set;
+        }
+
+        public ChatMessagePacket(PacketType pt)
+            : base(pt)
+        {
+
+        }
+
+        public override bool BuildPacket()
+        {
+            AddString(MessageSent);
+            return base.BuildPacket();
+        }
+    }
+}
