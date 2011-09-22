cde5a0c7fff6fa6e7f1eb7fec1df6caf673ef511
admin@glscripting.org <admin@glscripting.org@5e01b60c-9de4-9055-072c-856d3e03f645>
2011-09-22 05:26:57 +0000

    added DynamiclyMade.WebServer a library made to make websites using C# forms into a javascript/html/C# website.

---
+++ trunk/libopencraft/DynamicWebServer/FormToHtml/TextBox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using DynamicWebServer.special_html;
+
+namespace DynamicWebServer.FormToHtml
+{
+    public class HTMLTextBox : HtmlControl
+    {
+        public delegate byte[] ResponseRequested(string Variable, string Value);
+        public event ResponseRequested OnResponseRequested;
+        public CssScript cssScript = new CssScript();
+        string HTMLCode = "";
+        public HTMLTextBox(TextBox control)
+            : base(control)
+        {
+            this.Thiscontrol = control;
+            cssScript.html_Type = "div";
+            cssScript.type_Name = Thiscontrol.Name;
+            cssScript.sxml = special_xml.css;
+            cssScript.mime_Type = type.css;
+            cssScript.values = new string[] { (Thiscontrol.Bounds.X + (Thiscontrol.Bounds.X / 2)).ToString(), (Thiscontrol.Bounds.Y + (Thiscontrol.Bounds.Y / 2)).ToString(), "absolute", Thiscontrol.ClientRectangle.Height.ToString(), Thiscontrol.ClientRectangle.Width.ToString() };
+            cssScript.variables = new css_variables[] { css_variables.left, css_variables.top, css_variables.position, css_variables.height, css_variables.width };
+            //this.Thiscontrol.ClientRectangle.X
+            OnItemResponse += new FormItemResponse(HTMLTextBox_OnItemResponse);
+        }
+
+        byte[] HTMLTextBox_OnItemResponse(string Variable, string Value)
+        {
+           return OnResponseRequested(Variable, Value);
+        }
+        
+        public override string PrintAllHTML()
+        {
+            return base.PrintAllHTML();
+        }
+        public string GetPageCode(string CurrentPage)
+        {
+            string cssSRipt = special_html.Make_Special.MakeCssScript(cssScript);
+            string TempStr = CurrentPage;
+            if (TempStr.IndexOf("<body>") != -1)
+            {
+                int test = TempStr.IndexOf("<body>");
+
+                string BeforeBody = TempStr.Substring(0, (TempStr.IndexOf("<body>") - 1));
+                string AfterBodyCode = "<div class=" + Thiscontrol.Name + "><input class=" + Thiscontrol.Name + " type=" + FormTypes.textbox + " value=\"" + Thiscontrol.Text + "\" /></div>" + "<body>";
+
+                string HtmlAfterCode = TempStr.Substring((TempStr.IndexOf("<body>") + 7));
+
+                TempStr = cssSRipt + BeforeBody + AfterBodyCode + HtmlAfterCode;
+            }
+            return TempStr;
+        }
+    }
+}
---
+++ trunk/libopencraft/DynamicWebServer/special_html/css.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace DynamicWebServer.special_html
+{
+    public class CssScript
+    {
+        public special_xml sxml;
+        public type mime_Type;
+        public string html_Type;
+        public string type_Name;
+        public css_variables[] variables;
+        public string[] values;
+
+        public CssScript(special_xml Sxml, type Mime_Type, string html_type, string type_name, css_variables[] Variables, string[] Values)
+        {
+            sxml = Sxml;
+            mime_Type = Mime_Type;
+            html_Type = html_type;
+            type_Name = type_name;
+            variables = Variables;
+            values = Values;
+        }
+        public CssScript()
+        {
+            sxml = new special_xml();
+            mime_Type = new type();
+            html_Type = "";
+            type_Name = "";
+            variables = new css_variables[] { };
+            values = new string[] { };
+        }
+    }
+    public static class Make_Special
+    {
+        public static string MakeJavaScript(string functionName, object[] Enums, string[] Values)
+        {
+            Enum[] TEnum = (Enum[])(Enums);
+            int i = 0;
+
+            string tStrOpen = "<" + EnumUtil.GetDescription( special_xml.javascript ) + " type=" + EnumUtil.GetDescription(type.javascript) + ">";
+            string tStrMiddle = "";
+            tStrMiddle = "function " + functionName + "() {" + "\n";
+            bool VarObj = false;
+            int VarObjPos = 0;
+            foreach (Enum TempE in TEnum)
+            {
+                if (EnumUtil.GetDescription(TempE).IndexOf('(') != -1 && VarObjPos != 1)
+                {
+                    VarObj = false;
+                    VarObjPos = 0;
+                    tStrMiddle += EnumUtil.GetDescription(TempE).Substring(0, EnumUtil.GetDescription(TempE).IndexOf('(')) + "(\""+Values[i]+"\");\n";
+                }
+                else if (EnumUtil.GetDescription(TempE).IndexOf('=') != -1 && VarObjPos != 1)
+                {
+                    VarObj = false;
+                    VarObjPos = 0;
+                    tStrMiddle += EnumUtil.GetDescription(TempE) + "\"" + Values[i] + "\";" + "\n";
+                }
+
+                else if (EnumUtil.GetDescription(TempE).IndexOf(' ') != -1 && VarObjPos != 1)
+                {
+                    tStrMiddle += EnumUtil.GetDescription(TempE) + Values[i] + " =" + " ";
+                    VarObjPos++;
+                }
+                else if (VarObjPos == 1)
+                {
+                    tStrMiddle += EnumUtil.GetDescription(TempE) + ";" + "\n";
+                    VarObjPos = 0;
+                }
+                
+                if (i == (Enums.Count() - 1))
+                {
+                    tStrMiddle += "}";
+                }
+                i++;
+            }
+
+            string tStrEnd = "</" + EnumUtil.GetDescription(special_xml.javascript) + ">";
+            return tStrOpen + "\n" + tStrMiddle + "\n" + tStrEnd + "\n";
+        }
+        public static string MakeCssScript(special_xml Sxml, type Mime_Type, string html_type, string type_name, css_variables[] Variables, string[] Values)
+        {
+            string tStrOpen = "<" + EnumUtil.GetDescription(Sxml) + " type=" + EnumUtil.GetDescription(Mime_Type) + ">";
+            string tStrMiddle = "";
+            if (html_type != "#")
+            {
+                tStrMiddle = "" + html_type + "." + type_name + " {"+"\n";
+            }
+            else
+            {
+                
+                tStrMiddle = "" + html_type + type_name + " {"+"\n";
+            }
+            int i = 0;
+
+            foreach (css_variables str in Variables)
+            {
+                tStrMiddle += EnumUtil.GetDescription(str) + ":" + Values[i] + ";" + "\n";
+                if(i == (Variables.Count() - 1))
+                {
+                    tStrMiddle += "}";
+                }
+                i++;
+            }
+
+            string tStrEnd = "</" + EnumUtil.GetDescription(Sxml) + ">";
+            return tStrOpen + "\n" + tStrMiddle + "\n" + tStrEnd + "\n";
+        }
+        public static string MakeCssScript(CssScript css)
+        {
+
+            string tStrOpen = "<" + EnumUtil.GetDescription(css.sxml) + " type=" + EnumUtil.GetDescription(css.mime_Type) + ">";
+            string tStrMiddle = "";
+            if (css.html_Type != "#")
+            {
+                tStrMiddle = "" + css.html_Type + "." + css.type_Name + " {" + "\n";
+            }
+            else
+            {
+                tStrMiddle = "" + css.html_Type + css.type_Name + " {" + "\n";
+            }
+            int i = 0;
+
+            foreach (css_variables str in css.variables)
+            {
+                tStrMiddle += EnumUtil.GetDescription(str) + ":" + css.values[i] + ";" + "\n";
+                if (i == (css.variables.Count() - 1))
+                {
+                    tStrMiddle += "}";
+                }
+                i++;
+            }
+
+            string tStrEnd = "</" + EnumUtil.GetDescription(css.sxml) + ">";
+            return tStrOpen + "\n" + tStrMiddle + "\n" + tStrEnd + "\n";
+        }
+    }
+    public static class javascript
+    {
+
+    }
+    public class StringValueAttribute : System.Attribute
+    {
+        private static Dictionary<Enum, object> _Values = new Dictionary<Enum, object>();
+        private string _value;
+
+        public StringValueAttribute(string value)
+        {
+            //_Values.Add((Enum)(this), value);
+            _value = value;
+        }
+
+        public string Value
+        {
+            get { return _value; }
+        }
+    }
+    public static class EnumUtil
+    {
+        public static string GetDescription(Enum en)
+        {
+
+            Type type = en.GetType();
+
+            MemberInfo[] memInfo = type.GetMember(en.ToString());
+
+            if (memInfo != null && memInfo.Length > 0)
+            {
+
+                object[] attrs = memInfo[0].GetCustomAttributes(typeof(StringValueAttribute),
+                                                                false);
+
+                if (attrs != null && attrs.Length > 0)
+
+                    return ((StringValueAttribute)attrs[0]).Value;
+
+            }
+
+            return en.ToString();
+
+        }
+    }
+    public enum special_xml : int
+    {
+        [StringValueAttribute("style")]
+        css = 0,
+        [StringValueAttribute("script")]
+        javascript = 1,
+    };
+    public enum css_variables
+    {
+        [StringValueAttribute("padding-bottom")]
+        padding_bottom,
+        [StringValueAttribute("padding-top")]
+        padding_top,
+        [StringValueAttribute("padding-right")]
+        padding_right,
+        [StringValueAttribute("padding-left")]
+        padding_left,
+        [StringValueAttribute("margin-left")]
+        margin_left,
+        [StringValueAttribute("margin-right")]
+        margin_right,
+        [StringValueAttribute("margin-top")]
+        margin_top,
+        [StringValueAttribute("margin-bottom")]
+        margin_bottom,
+        [StringValueAttribute("border")]
+        border,
+        [StringValueAttribute("height")]
+        height,
+        [StringValueAttribute("width")]
+        width,
+        [StringValueAttribute("left")]
+        left,
+        [StringValueAttribute("top")]
+        top,
+        [StringValueAttribute("bottom")]
+        bottom,
+        [StringValueAttribute("right")]
+        right,
+        [StringValueAttribute("float")]
+        Float,
+        [StringValueAttribute("text-align")]
+        text_align,
+        [StringValueAttribute("position")]
+        position,
+    };
+    public enum type
+    {
+        [StringValueAttribute("text/css")]
+        css = 0,
+        [StringValueAttribute("text/javascript")]
+        javascript = 1,
+    }
+    public enum jsDocument
+    {
+    }
+    public enum ajaxCalls
+    {
+        [StringValueAttribute("XMLHttpRequest()")]
+        XMLHttpRequest,
+    }
+    public enum jsWindow
+    {
+        [StringValueAttribute("window.location.href=")]
+        window_location_href,
+        [StringValueAttribute("window.location.reload()")]
+        window_location_reload,
+        [StringValueAttribute("window.location.reload()")]
+        window_location_replace,
+    };
+    public enum CommonJs
+    {
+        [StringValueAttribute("location.href=")]
+        location_href,
+        [StringValueAttribute("var ")]
+        var,
+    }
+}
