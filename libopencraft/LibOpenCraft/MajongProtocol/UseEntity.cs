4cbe62d564a773041bccf6002ddddbc21073cc00
admin <admin@glscripting.org>
2011-10-17 12:35:01 +0000

    added use entity

---
+++ libopencraft/LibOpenCraft/MajongProtocol/UseEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+
+using LibOpenCraft.ServerPackets;
+
+namespace LibOpenCraft.MajongProtocol
+{
+    [Export(typeof(CoreEventModule))]
+    [ExportMetadata("Name", "UseEntity")]
+    class UseEntity : CoreEventModule
+    {
+        string name = "";
+        public UseEntity()
+            : base(PacketType.UseEntity)
+        {
+
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            ModuleHandler.AddEventModule(PacketType.UseEntity, new ModuleCallback(OnUseEntity));
+            base.RunModuleCache();
+        }
+
+        public void OnUseEntity(ref PacketReader _pReader, PacketType pt, ref ClientManager _client)
+        {
+            _pReader.ReadInt();
+            _pReader.ReadInt();
+            _pReader.ReadByte();
+        }
+
+        public override void Stop()
+        {
+            base.Stop();
+            ModuleHandler.RemoveEventModule(PacketType.UseEntity);
+        }
+    }
+}
